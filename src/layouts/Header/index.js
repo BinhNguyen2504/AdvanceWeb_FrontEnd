4e34a786ad07cb189ae0da903249d5af45622445
BinhNguyen2504 <ngvanbinh2001@gmail.com>
2022-12-01 13:39:35 +0700

    Install dark mode

--- src/layouts/Header/index.js
+++ src/layouts/Header/index.js
@@ -1,41 +1,75 @@
 import { Layout } from 'antd';
+import classNames from 'classnames';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 
 const { Header: HeaderAnt } = Layout;
 
-const Header = () => (
-  <HeaderAnt className='header'>
-    <section className='flex'>
-      <a href='/' className='logo'>
-        Kahoot
-      </a>
+const Header = () => {
+  const { username, email } = useSelector((state) => state.auth);
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+  useEffect(() => {
+    if (theme === 'light') {
+      localStorage.setItem('theme', JSON.stringify('light'));
+    }
+    if (theme === 'dark') {
+      document.getElementById('toggle-btn').classList.replace('fa-sun', 'fa-moon');
+      document.body.classList.add('dark');
+    } else {
+      document.getElementById('toggle-btn').classList.replace('fa-moon', 'fa-sun');
+      document.body.classList.remove('dark');
+    }
+  }, [theme]);
+  const handleChangeTheme = () => {
+    localStorage.setItem('theme', JSON.stringify(theme === 'dark' ? 'light' : 'dark'));
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
 
-      <div className='icons'>
-        <div id='menu-btn' className='fas fa-bars' />
-        <div id='search-btn' className='fas fa-search' />
-        <div id='user-btn' className='fas fa-user' />
-        <div id='toggle-btn' className='fas fa-sun' />
-      </div>
+  const [toggleProfile, setToggleProfile] = useState(true);
 
-      <div className='profile'>
-        <img src='images/pic-1.jpg' className='image' alt='' />
-        <h3 className='name'>Binh nguyen</h3>
-        <p className='role'>students</p>
-        <a href='profile.html' className='btn'>
-          view profile
-        </a>
-        <div className='flex-btn'>
-          <a href='login.html' className='option-btn'>
-            login
-          </a>
-          <a href='register.html' className='option-btn'>
-            register
-          </a>
+  return (
+    <HeaderAnt className='header'>
+      <section className='flex'>
+        <Link to='/' className='logo'>
+          Kahoot
+        </Link>
+
+        <div className='icons'>
+          <div id='menu-btn' className='fas fa-bars' />
+          <div id='search-btn' className='fas fa-search' />
+          <button
+            id='user-btn'
+            className='fas fa-user'
+            onClick={() => setToggleProfile(!toggleProfile)}
+            type='button'
+            aria-label='Toggle profile'
+          />
+          <button
+            id='toggle-btn'
+            className='fas fa-sun'
+            onClick={handleChangeTheme}
+            type='button'
+            aria-label='Toggle theme'
+          />
         </div>
-      </div>
-    </section>
-  </HeaderAnt>
-);
 
+        <div
+          className={classNames('profile', {
+            active: toggleProfile
+          })}
+        >
+          <img src='img/ava.jpg' className='image' alt='' />
+          <h3 className='name'>{username}</h3>
+          <p className='role'>{email}</p>
+          <button type='button' className='btn'>
+            Logout
+          </button>
+        </div>
+      </section>
+    </HeaderAnt>
+  );
+};
 export default Header;
--- src/layouts/SideBar/index.js
+++ src/layouts/SideBar/index.js
@@ -1,44 +1,37 @@
-const SideBar = () => (
-  <div className='side-bar'>
-    <div id='close-btn'>
-      <i className='fas fa-times' />
-    </div>
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-    <div className='profile'>
-      <img
-        src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png'
-        className='image'
-        alt=''
-      />
-      <h3 className='name'>Binh Nguyen</h3>
-      <p className='role'>Student</p>
-      <a href='/profile' className='btn'>
-        view profile
-      </a>
-    </div>
+const SideBar = () => {
+  const { username, email } = useSelector((state) => state.auth);
 
-    <nav className='navbar'>
-      <a href='/'>
-        <i className='fas fa-home' />
-        <span>home</span>
-      </a>
-      <a href='about.html'>
-        <i className='fas fa-question' />
-        <span>about</span>
-      </a>
-      <a href='/groups'>
-        <i className='fas fa-graduation-cap' />
-        <span>groups</span>
-      </a>
-      <a href='teachers.html'>
-        <i className='fas fa-chalkboard-user' />
-        <span>teachers</span>
-      </a>
-      <a href='contact.html'>
-        <i className='fas fa-headset' />
-        <span>contact us</span>
-      </a>
-    </nav>
-  </div>
-);
+  return (
+    <div className='side-bar'>
+      <div id='close-btn'>
+        <i className='fas fa-times' />
+      </div>
+      <div className='profile'>
+        <img src='/img/ava.jpg' className='image' alt='' />
+        <h3 className='name'>{username}</h3>
+        <p className='role'>{email}</p>
+        <Link to='/profile' className='btn'>
+          view profile
+        </Link>
+      </div>
+      <nav className='navbar'>
+        <Link to='/'>
+          <i className='fas fa-home' />
+          <span>Dashboard</span>
+        </Link>
+        <Link to='/groups'>
+          <i className='fas fa-chalkboard-user' />
+          <span>Groups</span>
+        </Link>
+        <Link to='/'>
+          <i className='fas fa-headset' />
+          <span>Contact us</span>
+        </Link>
+      </nav>
+    </div>
+  );
+};
 export default SideBar;
